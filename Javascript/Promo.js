6d50552029e1429db0848766a2318af4357e4da0
Pedro <raimundo.pedro2004@gmail.com>
2024-06-20 15:52:12 +0100

    Promo por fazer

--- Javascript/Promo.js
+++ Javascript/Promo.js
@@ -1,14 +1,37 @@
-let slideIndex = 0;
-const slides = document.getElementsByClassName("slides");
+const slides = document.querySelectorAll(".slides img");
+    let slideIndex = 0;
+    let intervalId = null;
 
-function showSlides() {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+    document.addEventListener("DOMContentLoaded", initializeSlider);
+
+    function initializeSlider(){
+        if(slides.length > 0){
+            slides[slideIndex].classList.add("displaySlide");
+            intervalId = setInterval(nextSlide, 5000);
+        }
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
-}
 
-showSlides(); // Inicia o slideshow
+    function showSlide(index){
+        if(index >= slides.length){
+            slideIndex = 0;
+        }
+        else if(index < 0){
+            slideIndex = slides.length -1;
+        }
+
+        slides.forEach(slide => {
+            slide.classList.remove("displaySlide");
+        });
+        slides[slideIndex].classList.add("displaySlide");
+    }
+
+    function prevSlide(){
+        clearInterval(intervalId);
+        slideIndex--;
+        showSlide(slideIndex);
+    }
+
+    function nextSlide(){
+        slideIndex++;
+        showSlide(slideIndex);
+    }
