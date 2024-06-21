ee5d1abfa0b0304e73a5549754ad2aa140143b68
Daniel <152877856+Dicixar@users.noreply.github.com>
2024-06-21 10:21:44 +0100

    Projeto V1

--- Javascript/Promo.js
+++ Javascript/Promo.js
@@ -1,70 +1,67 @@
 const slides = document.querySelectorAll(".slides img");
-    let slideIndex = 0;
-    let intervalId = null;
+let slideIndex = 0;
+let intervalId = null;
 
-    document.addEventListener("DOMContentLoaded", initializeSlider);
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
-    function initializeSlider(){
-        if(slides.length > 0){
-            slides[slideIndex].classList.add("displaySlide");
-            intervalId = setInterval(nextSlide, 5000);
-        }
-    }
-
-    function showSlide(index){
-        if(index >= slides.length){
-            slideIndex = 0;
-        }
-        else if(index < 0){
-            slideIndex = slides.length -1;
-        }
-
-        slides.forEach(slide => {
-            slide.classList.remove("displaySlide");
-        });
+function initializeSlider() {
+    if (slides.length > 0) {
         slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
     }
 
-    function prevSlide(){
-        clearInterval(intervalId);
-        slideIndex--;
-        showSlide(slideIndex);
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+if (user) {
+    // Utilizador está logado, mostra o botão de Logout
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('profile').style.display = '';
+
+
+} else {
+    // Utilizador não está logado, mostra o botão de Login
+    document.getElementById('login').style.display = '';
+    document.getElementById('profile').style.display = 'none';
+}
+function Profile() {
+    if (user.name == 'admin') {
+        window.location.replace('LoginAdmin.html');
     }
-
-    function nextSlide(){
-        slideIndex++;
-        showSlide(slideIndex);
+    else if (user) {
+        window.location.replace('LoginUser.html');
     }
-
-    const user = JSON.parse(localStorage.getItem('user'));
-
-    if (user) {
-        // Atualiza a mensagem de boas-vindas
-        document.getElementById('1').textContent = 'Bem-vindo à MotorSeguros, ' + user.name ;
-
-        // Utilizador está logado, mostra o botão de Logout
-        document.getElementById('login').style.display = 'none';
-        document.getElementById('profile').style.display = '';
-
-
-    } else {
-        // Utilizador não está logado, mostra o botão de Login
-        document.getElementById('login').style.display = '';
-        document.getElementById('profile').style.display = 'none';
+    else {
+        window.location.replace('Login.html');
     }
-    function Profile() {
-        if (user.name == 'admin') {
-            window.location.replace('LoginAdmin.html');
-        }
-        else if (user) {
-            window.location.replace('LoginUser.html');
-        }
-        else {
-            window.location.replace('Login.html');
-        }
-    }
+}
 
-    function toggleMenu() {
-        const menuList = document.getElementById('menu-list');
-        menuList.classList.toggle('show');
-    }
+function toggleMenu() {
+    const menuList = document.getElementById('menu-list');
+    menuList.classList.toggle('show');
+}
